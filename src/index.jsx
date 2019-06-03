974af0e30239503e2ccdc1c354e590a5604932cd
ihlee <leeinhwan7@dgist.ac.kr>
2019-06-03 11:18:19 +0900

    React router
    - Add react-router-dom
    - Add history
    - Delete App.jsx
    - Add Home.jsx
    - Add Login.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import {
+  BrowserRouter as Router, Route, Redirect, Switch,
+} from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
+import Home from './pages/Home';
+import Login from './pages/Login';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const customHistory = createBrowserHistory();
+const Root = () => (
+  <Router history={customHistory}>
+    <Switch>
+      <Route path="/login" component={Login} />
+      <Route path="/app/home" component={Home} />
+      <Redirect exact from="/" to="/login" />
+    </Switch>
+  </Router>
+);
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/pages/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Login() {
+  return <LoginPage />;
+}
+
+const LoginPage = () => (
+  <div>
+    <h1>Login</h1>
+    <div>
+          Login Page
+    </div>
+  </div>
+);
